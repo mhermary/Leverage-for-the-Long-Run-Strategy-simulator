dfc29ef1648cc96aab5d487d509502a40dc00f1b
Matthew Hermary <matthermary@gmail.com>
2024-12-19 23:46:05 -0800

    Cleaned up code, removed unnecessary comments

--- 200DMA LRS yfinance.py
+++ 200DMA LRS yfinance.py
@@ -5,30 +5,25 @@ import math
 import yfinance as yf
 import matplotlib.dates as mdates
 
-# When making trades, sell first then buy
-# include TMF on contrary to LETF purchase/sell
 moving_avg = 100
 cash_only = 1
 
-# Step 1: Load historical price data (for example, CSV file)
-indx = yf.Ticker('^GSPC') # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
-letf = yf.Ticker('UPRO')
-tbill = yf.Ticker('SPDN')
+mrkt = '^GSPC'# index to follow - ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
+levETF = 'UPRO' # leveraged ETF to rotate into when the underlying index trades above the moving average
+hedge = 'SPDN' # Hedge to be rotated into when indx is under moving average. Only rotates in if cash_only is false - < 3mo tbill BIL, SHV, VGSH; Gold GLD, IAU; market short: SPDN, PSQ
+indx = yf.Ticker(mrkt) 
+letf = yf.Ticker(levETF)
+tbill = yf.Ticker(hedge)
 data = indx.history(period= 'max') # Index data to track
 data_tr = letf.history(period= 'max')
 data_tbill = tbill.history(period= 'max')
-# data = indx.history(start = "2005-07-01", end = "2020-12-31") # Index data to track
+# for custom date range
+# data = indx.history(start = "2005-07-01", end = "2020-12-31")
 # data_tr = letf.history(start = "2007-07-01", end = "2020-12-31")
 # data_tbill = tbill.history(start = "2007-07-01", end = "2020-12-31")
-# print(data)
-# print(data.head())  # For S&P 500 data
-# print(data_tr.head())  # For UPRO data
-# print(data_tbill.head())  # For TMF data
 
 #calcualte MA before aligning start dates to have MA at starting data
 close_prices = data[['Close']]
-# data['200MA'] = close_prices['Close'].rolling(window=200).mean()
-# data['50MA'] = close_prices['Close'].rolling(window=50).mean()
 data['MA'] = close_prices['Close'].rolling(window=moving_avg).mean()
 
 # setting all dataframes to have same start date
@@ -41,23 +36,11 @@ data = aligned_dfs[0]
 data_tr = aligned_dfs[1]
 data_tbill = aligned_dfs[2]
 
-# Step 2: Extract the moving average
-# close_prices = data[['Close']]
-# data['MA'] = close_prices['Close'].rolling(window=moving_avg).mean()
-
-# Step 3: Create buy/sell signals
-# Buy when price crosses above the moving average
-# data_tr['Buy'] = (data['Close'] > data['MA']) & (data['Close'].shift(1) <= data['MA'].shift(1))
-# data_tr['Buy'] = (data['Close'] > data['200MA'])
+# Buy/Sell when price is above/below the moving average
 data_tr['Buy'] = (data['Close'] > data['MA'])
-
-# Sell when price crosses below the moving average
-# data_tr['Sell'] = (data['Close'] < data['MA']) & (data['Close'].shift(1) >= data['MA'].shift(1))
-# data_tr['Sell'] = (data['Close'] < data['50MA'])
 data_tr['Sell'] = (data['Close'] < data['MA'])
 
-# Step 4: Backtest the strategy
-# Initialize variables
+# Backtest
 position = 0  # ETF Shares
 tb_position = 0 # tbill Shares
 capital = 10000  # initial capital
@@ -66,30 +49,22 @@ returns = []
 buydates = []
 selldates = []
 trades = 0
-# cash_only = 1
 holding_tbill = False
 in_tbill_date =  0 if cash_only == 1 else 1
 
 if cash_only:
     for index, row in data_tr.iterrows():
-        # Buy signal
+        # Buy
         if row['Buy'] and position == 0:
             buy_price = row['Close']
-            # tmp = data_tr.loc[index] 
-            # buy_price = tmp['<CLOSE>']
-            # print(f"Buying at {buy_price} on date {index}")
-            # buy_price = float(buy_price)
             position = capital / buy_price  # Buy the asset
-            # position = float(position)
             capital = 0
             buydates.append(index)
             trades = trades + 1
 
-        # Sell signal
+        # Sell
         elif row['Sell'] and position > 0:
             sell_price = row['Close']
-            # tmp = data_tr.loc[index] 
-            # buy_price = tmp['<CLOSE>']
             capital = position * sell_price  # Sell the asset
             position = 0
             # Calculate return and store it
@@ -109,7 +84,7 @@ else:
                 sell_price = data_tbill['Close'].loc[index]
                 capital += tb_position * sell_price
                 holding_tbill = False
-            # capital += tb_position*data_tbill['Dividends'].loc[index]
+            # capital += tb_position*data_tbill['Dividends'].loc[index] # most hedges gave minimal dividends, code is kept in case dividend hedge is chosen
             buy_price = row['Close']
             position = capital / buy_price
             capital = 0
@@ -139,7 +114,7 @@ else:
 
 print(capital_history)
 data['Capital'] = capital_history
-# Step 5: Calculate the performance
+# Calculate the performance
 if position > 0:
     sell_price = data_tr['Close'].iloc[-1] #iloc [-1] gives last value in <CLOSE>. ILOC + Integer location based indexing
     capital = position * sell_price #Liquidate assets to see final gains
@@ -171,39 +146,45 @@ print()
 # for j in selldates:
 #     print(j)
 
+# in case trades want to be kept to be reviewed externally
 # data['Transaction'] = data['Buy'] | data['Sell']
 # df = pd.DataFrame(data)
 # df.to_csv('50DMA_trades.csv', sep =',', index=False)
 
 if 1:
-    # Create a new figure
+    # new
     fig, ax1 = plt.subplots(figsize=(10, 5))
     fig, ax3 = plt.subplots(figsize=(10, 5))
     plot_MA = str(moving_avg) + " day MA"
     # Plot Close Price and 50-Day Moving Average on the first y-axis (ax1)
-    ax1.plot(data.index, data['Close'], label='Close Price', color='blue')
+    ax1.plot(data.index, data['Close'], label= mrkt, color='blue')
     ax1.plot(data.index, data['MA'], label=plot_MA, color='orange')
-    # ax1.plot(data.index, data['200MA'], label='200-Day MA', color='orange')
-    # ax1.plot(data.index, data['50MA'], label='50-Day MA', color='red')
     # ax1.scatter(buydates, data.loc[buydates]['Close'], marker='^', color='g', label='Buy Signal', alpha=1)
     # ax1.scatter(selldates, data.loc[selldates]['Close'], marker='v', color='r', label='Sell Signal', alpha=1)
     # Label for the first y-axis (for price)
-    ax1.set_ylabel('Price')
+    ax1.set_ylabel('Index Value')
     ax1.set_title('Moving Average LRS with Capital Over Time')
     ax1.legend(loc='upper left')
-    #Set dates to X-Axis
+    # Set dates to X-Axis
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # Format as Year-Month
     ax1.xaxis.set_major_locator(mdates.YearLocator())  # Major ticks at each year
     ax1.xaxis.set_minor_locator(mdates.MonthLocator())  # Minor ticks at each month
     # plt.xticks(rotation=45)  # Rotate date labels for better readability
+
     # Create a second y-axis (ax2) for capital history
     ax2 = ax1.twinx()
     ax2.plot(data.index, capital_history, label='Capital', color='purple', linestyle='--', alpha=0.7)
-    ax3.plot(data.index, data_tbill['Close'], label='T-Bill Price', color='red')
+    ax2.set_ylabel('Capital')
+    ax2.legend(loc='upper right')
+
+    # Create a second graph (ax3) for hedge history
+    ax3.plot(data.index, data_tbill['Close'], label= hedge, color='red')
+    ax3.set_ylabel('Price')
+    ax3.set_title('Hedge Price')
+    ax3.legend(loc='upper left')
     ax3.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # Format as Year-Month
     ax3.xaxis.set_major_locator(mdates.YearLocator())  # Major ticks at each year
     ax3.xaxis.set_minor_locator(mdates.MonthLocator())  # Minor ticks at each month
     # Label for the second y-axis (for capital)
-    ax2.set_ylabel('Capital')
-    ax2.legend(loc='upper right')
+
     plt.show()
--- Moving Average Plotter.py
+++ Moving Average Plotter.py
@@ -7,23 +7,20 @@ import matplotlib.dates as mdates
 moving_avg = 100
 cash_only = 0
 
-ticker1 = '^GSPC' # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
-ticker2 = '^NDX'
-period = '2y'
+ticker1 = 'CASH.TO' # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
+ticker2 = 'CASH.TO' 
+# Does not work well with ETFs that regularly pay dividends like BIL, SHV, VGSH, CASH.TO
+# Something in the yfinance data import does not lower the stock price after dividends are paid out
+period = '5y'
 
 indx = yf.Ticker(ticker1) # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
 alt = yf.Ticker(ticker2) # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
 data = indx.history(period = period) # Index data to track, '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
 alt_data = alt.history(period = period) # Index data to track
-
-
+# Hedge to be rotated into when indx is under moving average. Only rotates in if cash_only is false - < 3mo tbill BIL, SHV, VGSH; Gold GLD, IAU; market short: SPDN, PSQ
 # start = datetime.datetime(2019, 7, 1)
 # end = datetime.datetime.now()
 # tbill_data = web.DataReader('TB3MS', 'fred', start, end)  # 3-month T-Bill data
-
-
-close_prices = data[['Close']]
-# alt_prices = alt_data[['Close']]
 # tbill_prices = tbill_data
 data['MA'] = data['Close'].rolling(window=moving_avg).mean()
 
